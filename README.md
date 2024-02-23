3cea9eb6f921fa12bd06e00e232386ad2e391c4d
wangjiaqi <wangjiaqi@jwzg.com>
2024-02-23 11:39:16 +0800

    feat：一个项目

--- README.md
+++
@@ -1,1 +0,0 @@
-# Wiq-Study
---
+++ vue-demo/src/components/views/First.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="first_app">
+    {{ msg }}
+  </div>
+</template>
+<script>
+export default{
+  name: 'First',
+  data () {
+    return {
+      msg: 'Welcome'
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
